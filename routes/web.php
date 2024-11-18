0fabbf8914cc77b477fddd67e03898de4e4c4dff
Je12emy <jeremyzelaya@hotmail.es>
2024-11-17 20:52:10 -0600

    feat: add new controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::resource('blog', BlogController::class)
+    ->only(['index', 'show']);
