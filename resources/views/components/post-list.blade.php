803112a915873d5a9c6423331195fcc69944a9d8
Je12emy <jeremyzelaya@hotmail.es>
2024-11-24 18:58:40 -0600

    style: post styling

--- resources/views/components/post-list.blade.php
+++ resources/views/components/post-list.blade.php
@@ -1,10 +1,15 @@
 @props(['title', 'publication_date', 'slug'])
 
+@php
+use Carbon\Carbon;
+$date = Carbon::parse($publication_date)->toFormattedDateString();
+@endphp
+
 <a href="{{$slug}}">
-    <article class="flex flex-row space-x-2">
+    <article class="flex flex-row space-x-3">
         <time datetime="{{$publication_date}}" class="text-md font-light">
-            {{$publication_date}}
+            {{$date}}
         </time>
-        <header> {{$title}} </header>
+        <header class=""> {{$title}} </header>
     </article>
 </a>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,17 +2,22 @@
     @vite('resources/css/app.css')
 </head>
 
-<body class="bg-black text-white my-4">
+<body class="bg-black text-white h-screen">
     <article class="text-base">
         <x-post-header :title="$article->title" :publication_date="$article->publication_date" />
         {!! $content !!}
     </article>
-    <aside class="max-w-max">
-        <section class="flex flex-col space-y-2 ">
-            <header> Other Posts </header>
-            @foreach ($others as $other)
-            <x-post-list :title="$other->title" :publication_date="$other->publication_date" :slug="$other->slug" />
-            @endforeach
+    <aside class="mt-4">
+        <section>
+            <header class="font-bold text-lg mb-2"> Other Posts </header>
+            <ul class="flex flex-col border-l-gray-400 pl-3 border-l-2 space-y-2">
+                @foreach ($others as $other)
+                <li>
+                    <x-post-list :title="$other->title" :publication_date="$other->publication_date"
+                        :slug="$other->slug" />
+                </li>
+                @endforeach
+            </ul>
         </section>
     </aside>
 </body>
